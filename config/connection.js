6928d33615ee40415f2105e9e90a032b3dfb5e4b
Stephanie Minker <sminker0401@gmail.com>
2023-02-11 22:04:09 -0500

    created connection.js in config

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const { connect, connection } = require('mongoose');
+
+// Add your Atlas connection string as a Config variable, node looks for this variable and will use it if it exists. Otherwise, it will assume that you are running this application locally
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
